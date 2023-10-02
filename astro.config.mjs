e51f2b3fdd17d5075a21b0a8555db6d203a8dd12
MagickSpace <146006257+MagickSpace@users.noreply.github.com>
2023-10-02 19:48:43 +0000

    + Dbest-stack

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,9 @@ import react from "@astrojs/react";
 import yaml from "@rollup/plugin-yaml";
 import node from "@astrojs/node";
 import tailwind from "@astrojs/tailwind";
-
 import sitemap from "@astrojs/sitemap";
+
+import solidJs from "@astrojs/solid-js";
 
 // https://astro.build/config
 export default defineConfig({
@@ -14,18 +15,18 @@ export default defineConfig({
   },
   integrations: [react(), tailwind({
     applyBaseStyles: false
-  }), 
-  sitemap({
+  }), sitemap({
     i18n: {
-      defaultLocale: 'en', // All urls that don't contain `es` or `fr` after `https://stargazers.club/` will be treated as default locale, i.e. `en`
+      defaultLocale: 'en',
+      // All urls that don't contain `es` or `fr` after `https://stargazers.club/` will be treated as default locale, i.e. `en`
       locales: {
-        en: 'en-US', // The `defaultLocale` value must present in `locales` keys
+        en: 'en-US',
+        // The `defaultLocale` value must present in `locales` keys
         es: 'es-ES',
-        fr: 'fr-CA',
-      },
-    },
-  }),
-],
+        fr: 'fr-CA'
+      }
+    }
+  }), solidJs()],
   output: "server",
   adapter: node({
     mode: "standalone"
