3bbd11ef56734d547d15945a102500b5aefcec5c
MagickSpace <146006257+MagickSpace@users.noreply.github.com>
2023-10-02 16:13:17 +0000

    !

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,31 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
-
+import yaml from "@rollup/plugin-yaml";
 import node from "@astrojs/node";
+import tailwind from "@astrojs/tailwind";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  site: 'https://astrobunny.devopsick.com',
+  vite: {
+    plugins: [yaml()]
+  },
+  integrations: [react(), tailwind({
+    applyBaseStyles: false
+  }), 
+  sitemap({
+    i18n: {
+      defaultLocale: 'en', // All urls that don't contain `es` or `fr` after `https://stargazers.club/` will be treated as default locale, i.e. `en`
+      locales: {
+        en: 'en-US', // The `defaultLocale` value must present in `locales` keys
+        es: 'es-ES',
+        fr: 'fr-CA',
+      },
+    },
+  }),
+],
   output: "server",
   adapter: node({
     mode: "standalone"
